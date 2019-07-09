2765e2a1e4bca362a6521f2c209a8d28dbbdb3af
Kiru_Freecss <carloskportillo@gmail.com>
2019-07-09 00:00:46 -0600

    New Commit

--- public/js/app.js
+++ public/js/app.js
@@ -116,8 +116,8 @@ var gretting = 'hello';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Programacion\Desktop\commerce-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Programacion\Desktop\commerce-app\resources\scss\app.scss */"./resources/scss/app.scss");
+__webpack_require__(/*! C:\Users\kirup\OneDrive\Escritorio\commerce-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\kirup\OneDrive\Escritorio\commerce-app\resources\scss\app.scss */"./resources/scss/app.scss");
 
 
 /***/ })
